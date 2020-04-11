ea1b63ccb1063c737ed454a97891e62b5f3ec8dc
Dianne Gramling <lee@profilestudios.com>
2020-04-11 14:11:49 -0400

    added auth0 credentials to environment.ts; added errorhandlers to api.py finished todos in auth.py

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -225,5 +225,31 @@ def not_found(error):
     error handler should conform to general task above 
 '''
 
-@app.errorhandler(401)
-def
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+                    "success": False,
+                    "error": 401,
+                    "message": "cannot authenticate user"
+                    }), 401
+
+
+# @app.errorhandler(401)
+# def unauthorized(error):
+#     return jsonify({
+#                     "success": False,
+#                     "error": 401,
+#                     "message": "cannot authenticate user"
+#                     }), 401
+#
+
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({
+                    "success": False,
+                    "error": 403,
+                    "message": "you do not have permission to access this resource"
+                    }), 403
+
+
+
